caf7279619c4a713815fbde7b1a8a047f5e19d50
Brandon Orther <brandon.orther@gmail.com>
2015-12-31 10:31:35 -0800

    Added electron functions and tests.
    I wanted to add these tests to show that the actual Electron window that
    karma is running in is the current electron window. I considered adding
    tests to open a new Electron window but figured I could just write a
    blog post and/or create a example cljs Electron app repo that people can
    reference.

--- example/src/example/electron.cljs
+++ example/src/example/electron.cljs
@@ -1,5 +1,21 @@
 (ns example.electron)
 
 (def electron (js/require "electron"))
-(def app (.-app (.-remote electron)))
-(def electronScreen (.-screen electron))
+(def remote (.-remote electron ))
+(def app (.-app remote))
+
+(defn get-current-window
+  []
+  (.getCurrentWindow remote))
+
+(defn get-window-size
+  "Helper function to get Electron window size."
+  [window]
+  (let [[width height] (array-seq (.getSize window))]
+    {:width width
+     :height height}))
+
+(defn set-window-size!
+  "Helper function to set Electron window size."
+  [window {:keys [width height]}]
+  (.setSize window width height))
